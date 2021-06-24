c4ffc41835a7b17c4b294ee1a169cdae23af21cc
dishwasher-detergent <kenny_bass@outlook.com>
2021-06-24 18:05:04 -0500

    Update tailwind.config.js

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     './components/**/*.{vue,js}',
@@ -9,6 +11,19 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      // Build your palette here
+      white: '#ffffff',
+      black: '#000000',
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.blueGray,
+      red: colors.rose,
+      blue: colors.blue,
+      purple: colors.violet,
+      green: colors.emerald,
+      yellow: colors.amber,
+    }
   },
   variants: {
     extend: {},
