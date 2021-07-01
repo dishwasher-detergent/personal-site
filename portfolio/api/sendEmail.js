ffbb32c5819c90079f7132a76e9f529e69f747c8
dishwasher-detergent <kenny_bass@outlook.com>
2021-06-30 22:10:10 -0500

    Update sendEmail.js

--- portfolio/api/sendEmail.js
+++ portfolio/api/sendEmail.js
@@ -5,24 +5,21 @@ const password = process.env.NUXT_PASSWORD;
 export default async (req, res) => {
 
     const transporter = nodemailer.createTransport({
-        service: "hotmail",
+        host: 'smtp.live.com', // hostname
+        secureConnection: false, // use SSL
+        port: 587, // port for secure SMTP
         auth: {
             user: username,
             pass: password
+        },
+        tls:{
+            ciphers:'SSLv3'
         }
     });
 
-  transporter.verify(function(error, success) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Server is ready to take our messages");
-    }
-  });
-
   //2. You can configure the object however you want
   const mail = {
-    from: req.body.name.first + ' ' + req.body.name.last,
+    from: 'kenny.bass@outlook.com',
     to: 'kenny_bass@outlook.com',
     subject: req.body.subject,
     text: `${req.body.name.first + ' ' + req.body.name.last} <${req.body.email}> \n${req.body.message}`
