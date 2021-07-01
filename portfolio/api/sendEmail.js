88d32fad88e18c7b4deca1693f030f10599dce96
dishwasher-detergent <kenny_bass@outlook.com>
2021-06-30 22:10:28 -0500

    Update sendEmail.js

--- portfolio/api/sendEmail.js
+++ portfolio/api/sendEmail.js
@@ -3,29 +3,28 @@ const username = process.env.NUXT_USERNAME;
 const password = process.env.NUXT_PASSWORD;
 
 export default async (req, res) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.live.com", // hostname
+    secureConnection: false, // use SSL
+    port: 587, // port for secure SMTP
+    auth: {
+      user: username,
+      pass: password
+    },
+    tls: {
+      ciphers: "SSLv3"
+    }
+  });
 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.live.com', // hostname
-        secureConnection: false, // use SSL
-        port: 587, // port for secure SMTP
-        auth: {
-            user: username,
-            pass: password
-        },
-        tls:{
-            ciphers:'SSLv3'
-        }
-    });
-
-  //2. You can configure the object however you want
   const mail = {
-    from: 'kenny.bass@outlook.com',
-    to: 'kenny_bass@outlook.com',
+    from: "kenny.bass@outlook.com",
+    to: "kenny_bass@outlook.com",
     subject: req.body.subject,
-    text: `${req.body.name.first + ' ' + req.body.name.last} <${req.body.email}> \n${req.body.message}`
+    text: `${req.body.name.first + " " + req.body.name.last} <${
+      req.body.email
+    }> \n${req.body.message}`
   };
 
-  //3.
   transporter.sendMail(mail, (err, data) => {
     if (err) {
       console.log(err);
