11ccdc3dcb8d7412dece28fb2b4d6b10f6fd8510
dishwasher-detergent <kenny_bass@outlook.com>
2021-06-30 20:12:40 -0500

    Update sendEmail.js

--- portfolio/api/sendEmail.js
+++ portfolio/api/sendEmail.js
@@ -3,38 +3,40 @@ const username = process.env.NUXT_USERNAME;
 const password = process.env.NUXT_PASSWORD;
 
 export default async (req, res) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.live.com", //replace with your email provider
-    port: 587,
-    auth: {
-      user: username,
-      pass: password
-    }
-  });
+    console.log(req)
+    res.status(200).send("Email successfully sent to recipient!");
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp.live.com", //replace with your email provider
+//     port: 587,
+//     auth: {
+//       user: username,
+//       pass: password
+//     }
+//   });
 
-  transporter.verify(function(error, success) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Server is ready to take our messages");
-    }
-  });
+//   transporter.verify(function(error, success) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log("Server is ready to take our messages");
+//     }
+//   });
 
-  //2. You can configure the object however you want
-  const mail = {
-    from: req.name,
-    to: 'kenny_bass@outlook.com',
-    subject: req.subject,
-    text: `${req.name} <${req.email}> \n${req.message}`
-  };
+//   //2. You can configure the object however you want
+//   const mail = {
+//     from: req.name,
+//     to: 'kenny_bass@outlook.com',
+//     subject: req.subject,
+//     text: `${req.name} <${req.email}> \n${req.message}`
+//   };
 
-  //3.
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("Something went wrong.");
-    } else {
-      res.status(200).send("Email successfully sent to recipient!");
-    }
-  });
+//   //3.
+//   transporter.sendMail(mail, (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       res.status(500).send("Something went wrong.");
+//     } else {
+//       res.status(200).send("Email successfully sent to recipient!");
+//     }
+//   });
 };
