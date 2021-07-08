765fbe4a05712ca0295b0cf4d62e4b373b6e8ebf
dishwasher-detergent <33816169+dishwasher-detergent@users.noreply.github.com>
2021-07-08 15:18:02 -0500

    Update Login.vue

--- portfolio/pages/Login.vue
+++ portfolio/pages/Login.vue
@@ -1,119 +1,59 @@
 <template>
-  <form class="w-full flex flex-col" @submit.stop.prevent="userLogin">
-    <transition name="slide">
-      <div
-        class="
-          p-8
-          w-full
-          h-full
-          flex-1
-          rounded-2xl
-          shadow
-          bg-white
-          flex flex-col
-        "
-      >
-        <div class="space-y-4 flex-1">
-          <div
-            v-if="error"
-            class="
-              px-4
-              py-3
-              w-full
-              bg-red-200
-              text-red-600 text-bold
-              rounded-3xl
-            "
-          >
-            <span>{{ error }}</span>
-          </div>
-          <label v-if="type !== 'recovery'" class="flex flex-col font-bold">
-            <p class="text-sm text-gray-600 mb-2 ml-4">Username</p>
-            <input
-              v-model="login.email"
-              type="text"
-              placeholder="person@emailprovider.com"
-              class="py-3 px-4 rounded-2xl ring-1 ring-gray-300 text-lg"
-            />
-          </label>
-          <label class="flex flex-col font-bold">
-            <p class="text-sm text-gray-600 mb-2 ml-4">Password</p>
-            <input
-              v-model="login.password"
-              type="password"
-              placeholder="secretly secret"
-              class="py-3 px-4 rounded-2xl ring-1 ring-gray-300 text-lg"
-            />
-          </label>
-        </div>
-        <div class="mt-8 flex justify-end items-center">
-          <button
-            type="submit"
-            class="
-              w-24
-              py-2
-              px-4
-              inline-block
-              relative
-              text-center
-              rounded-3xl
-              bg-blue-600
-              text-white
-            "
-          >
-            <span v-if="type === 'recovery'">Reset</span>
-            <span v-else>Login</span>
-          </button>
-        </div>
-      </div>
-    </transition>
-  </form>
+    <div class="w-full">
+        <form v-if="type != 'signOut'" @submit.stop.prevent="userLogin" class="h-full w-full md:w-lg max-w-lg p-4 md:p-16">
+            <div class="w-full h-full rounded-2xl ring-1 ring-base-300 p-8 bg-base-100">
+                <h1 class="font-bold text-3xl pb-8">Log In</h1>
+                <div v-if="error" class="alert alert-error">
+                  <div class="flex-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current"><!----> <!----> <!----> <!----> 
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!----> <!---->
+                    </svg> 
+                    <label>{{error}}</label>
+                  </div>
+                </div>
+                <div class="form-control pb-8">
+                  <label class="label" for="email">
+                    <span class="label-text">Email</span>
+                  </label> 
+                  <input v-model="login.email" id="email" type="text" placeholder="Email" class="input input-bordered" required>
+                  <label class="label" for="password">
+                    <span class="label-text">Password</span>
+                  </label> 
+                  <input v-model="login.password" id="password" type="password" placeholder="Password" class="input input-bordered" required>
+                </div>
+                <div class="w-full flex items-center justify-center">
+                    <button aria-label="Login" :class="'btn btn-primary ' + (loading ? 'loading' : '')">
+                        Log In
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
 </template>
 <script>
 export default {
   data() {
     return {
       login: {
-        email: "",
-        password: "",
+        email: '',
+        password: ''
       },
       error: null,
-      type: null,
-      access_token: null,
-    };
+      loading: false
+    }
   },
   async mounted() {
-    let orig_hash = this.$route.hash;
-    if (orig_hash) {
-      this.type = "recovery";
-      const params = orig_hash.split("#")[1];
-      const paramsObj = Object.fromEntries(
-        new URLSearchParams(params).entries()
-      );
-      this.access_token = paramsObj.access_token;
-    } else {
-      const { type = null } = this.$route.query;
-      if (type === "signOut") {
+    const { type = null } = this.$route.query;
+    if (type && (type === 'signIn' || type === 'signUp' || type === 'signOut')) {
+      this.type = type;
+      if (type === 'signOut') {
         await this.signOut()
       }
     }
   },
   methods: {
     async userLogin() {
-      if (this.type === "recovery") {
-        this.resetPass();
-      } else {
-        this.loginUser();
-      }
-    },
-    async signOut() {
-      try {
-        let response = await this.$auth.logout('supabase')
-      } catch (err) {
-        console.log(err)
-      }
-    },
-    async loginUser() {
+      this.loading = true
       try {
         let response = await this.$auth.loginWith('supabase', 
           {
@@ -124,19 +64,15 @@ export default {
       } catch (err) {
         this.error = err.message
       }
+      this.loading = false
     },
-    async resetPass() {
-      try {
-        const { error, data } = await this.$supabase.auth.api.updateUser(
-          this.access_token,
-          {
-            password: this.login.password,
-          }
-        )
+    async signOut() {
+       try {
+        let response = await this.$auth.logout('supabase')
       } catch (err) {
-        this.error = err.message;
+        console.log(err)
       }
     },
-  },
-};
+  }
+}
 </script>
