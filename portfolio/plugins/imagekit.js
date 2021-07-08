9b112ce70ba70abb577bb70defc3b13565c7fec9
dishwasher-detergent <33816169+dishwasher-detergent@users.noreply.github.com>
2021-07-08 11:30:47 -0500

    Update imagekit.js

--- portfolio/plugins/imagekit.js
+++ portfolio/plugins/imagekit.js
@@ -8,5 +8,5 @@ const imageKit_endpoint = process.env.IMAGEKIT_ENDPOINT;
 Vue.use(ImageKit, {
   urlEndpoint: imageKit_endpoint,
   publicKey: public_api,
-  authenticationEndpoint: 'https://branandtate.com/api/imageKit'
+  authenticationEndpoint: 'https://kennethbass.com/api/imageKit'
 })
